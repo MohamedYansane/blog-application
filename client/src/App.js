ffc5355c4730132a444bed1ad42eef692dc317c5
Mohamed Yansane <yansane472@gmail.com>
2023-07-12 01:59:13 +0000

    updating files

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Footer } from "./components/footer/Footer";
 import { ArticleDetail } from "./pages/articleDetail/ArticleDetail";
 import { HomePage } from "./pages/home/HomePage";
 import { Routes, Route } from "react-router-dom";
+import { RegisterPage } from "./pages/register/RegisterPage";
 const App = () => {
   return (
     <div className="App font-jetbrain">
       <Routes>
         <Route index path="/" element={<HomePage />} />{" "}
         <Route path="blog/:id" element={<ArticleDetail />} />
+        <Route index path="/register" element={<RegisterPage />} />{" "}
       </Routes>
     </div>
   );
