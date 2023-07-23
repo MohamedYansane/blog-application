8e646dce56a8390859b3a32bb5946878148aa48b
Mohamed Yansane <yansane472@gmail.com>
2023-07-23 23:04:58 +0000

    updating the generateJwt method

--- server/models/users.js
+++ server/models/users.js
@@ -1,46 +1,50 @@
-import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken';
-const User = mongoose.Schema({
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+const User = mongoose.Schema(
+  {
     avatar: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     name: {
-        type: String,
-        required: [true, "field is required"]
+      type: String,
+      required: [true, "field is required"],
     },
     email: {
-        type: String,
-        required: [true, "field is required"]
+      type: String,
+      required: [true, "field is required"],
     },
     password: {
-        type: String,
-        required: [true, "field is required"]
+      type: String,
+      required: [true, "field is required"],
     },
     verified: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     verificationCode: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     admin: {
-        type: Boolean,
-        default: false
-    }
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+// je modifie le token de generer a partir du id et nom
+//si j'utilise l'arrow function avec this il n'instanciera  aucune donnée de notre
+//modele à mins que je n'utilise function instead of arrow
+User.methods.generateJwt = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+    },
+    process.env.ACCESS_TOKEN,
+    { expiresIn: "30d" }
+  );
+};
 
-}, {timestamps: true});
-
-User.methods.generateJwt = () => {
-    const accessToken = jwt.sign({
-        _id: this._id,
-        avatar: this.avatar,
-        name: this.name,
-        email: this.email,
-        password: this.password,
-        verified: this.verified
-    }, process.env.ACCESS_TOKEN, {expiresIn: "30d"});
-    return accessToken;
-}
 export default mongoose.model("Users", User);
