a9b34e53887c7aca4a4a12779033f4a4184b02b7
Mohamed Yansane <yansane472@gmail.com>
2023-07-30 20:54:33 +0000

    implementing the delete profile

--- client/src/components/ProfilePicture.jsx
+++ client/src/components/ProfilePicture.jsx
@@ -3,10 +3,18 @@ import { AddAPhoto, CameraAltOutlined } from "@mui/icons-material";
 import { stables } from "./constants";
 import { CropEasy } from "./crop/CropEasy";
 import { createPortal } from "react-dom";
+import { toast } from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateProfilePicture } from "../services/index/users";
+import { useDispatch, useSelector } from "react-redux";
+import { userAction } from "../store/reducers/userReducers";
 
 export const ProfilePicture = ({ avatar }) => {
   const [openCrop, setOpenCrop] = useState(false);
   const [photo, setPhoto] = useState(false);
+  const userState = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const queryClient = useQueryClient();
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     console.log(file);
@@ -14,6 +22,47 @@ export const ProfilePicture = ({ avatar }) => {
     //on i set the photo i want to setOpenCrop to true
     setOpenCrop(true);
     console.log(openCrop);
+  };
+
+  const { mutate, isLoading } = useMutation({
+    mutationFn: ({ token, formData }) => {
+      return updateProfilePicture({
+        token: token,
+        formData: formData,
+      });
+    },
+    onSuccess: (data) => {
+      dispatch(userAction.setUserInfo(data));
+      setOpenCrop(false);
+      localStorage.setItem("account", JSON.stringify(data));
+      queryClient.invalidateQueries(["profile"]);
+      toast.success("Profile Photo is removed");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+      console.log(error);
+    },
+  });
+
+  const handleDeleteImage = () => {
+    let text = "Do u wanna delete ur profile picture";
+    if (window.confirm(text)) {
+      try {
+        const formData = new FormData();
+        //in our postman our key for picture is profilePicture
+        //notice in our update profile in controller when there's no file
+        //we handle it in else so here we send an undefined instead of file
+        formData.append("profilePicture", undefined);
+
+        mutate({ token: userState.userInfo.token, formData: formData });
+      } catch (error) {
+        toast.error(error.message);
+        //and log the error object
+        console.log(error);
+      }
+    } else {
+      toast.success("Profile Photo is not removed");
+    }
   };
   return (
     <>
@@ -55,6 +104,7 @@ export const ProfilePicture = ({ avatar }) => {
           />
         </div>
         <button
+          onClick={handleDeleteImage}
           type="button"
           className="border border-red-500 rounded-lg px-4 py-2 text-red-500"
         >
