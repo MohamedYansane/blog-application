6741fff4149cd09aba6300cc3de5a6cf291dc377
Mohamed Yansane <yansane472@gmail.com>
2023-07-30 20:54:01 +0000

    reslving the bug of only PNG jpeg & file too large file

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,25 +1,23 @@
 import multer from "multer";
 import path from "path";
 const storage = multer.diskStorage({
-    destination: (req, file, cb) =>{
-        cb(null,path.join(__dirname,"../uploads"));
-    },
-    filename:(req, file, cb) =>{
-        cb(null, `${Date.now()}-${file.originalname}`);
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 export const upload = multer({
-    storage: storage,
-    fileSize:{
-        limits:1 * 1000000 //MB
-    },
-    fileFilter:(req,file,cb) =>{
-        let ext = path.extname(file.originalname);
-        if(ext !== ".png" || ext !== ".jpg" || ext !== ".jpeg")
-        {
-            cb(new Error("Only PNG , JPG  and JPEG files are allowed"));
-        }
-        cb(null,true);
+  storage: storage,
+  fileSize: {
+    limits: 1 * 1000000, //MB
+  },
+  fileFilter: (req, file, cb) => {
+    let ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+      cb(new Error("Only PNG , JPG  and JPEG files are allowed"));
     }
-    
+    cb(null, true);
+  },
 });
