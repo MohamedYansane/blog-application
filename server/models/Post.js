892a7406ad920234727e44912577f00ad376ea4c
Mohamed Yansane <yansane472@gmail.com>
2023-08-01 01:31:52 +0000

    updating a post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,37 +1,53 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const Post = mongoose.Schema(
+const postSchema = mongoose.Schema(
   {
     title: {
       type: String,
-      default: "",
+      required: true,
     },
-    name: {
+    caption: {
       type: String,
-      required: [true, "field is required"],
+      required: true,
     },
-    email: {
+    //we need the slug for our article url it's must be unique
+    slug: {
       type: String,
-      required: [true, "field is required"],
+      required: true,
+      unique: true,
     },
-    password: {
+    //Why Object cause we r using editor js and it returns an object
+    body: {
+      type: Object,
+      required: true,
+    },
+    photo: {
       type: String,
-      required: [true, "field is required"],
+      required: true,
     },
-    verified: {
-      type: Boolean,
-      default: false,
+    // cause we wanna know which user added the post
+    //with the ref we can now populate our data
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "Users",
     },
-    verificationCode: {
-      type: String,
-      required: false,
+    //tags is equal to an array of string
+    tags: {
+      type: [String],
     },
-    admin: {
-      type: Boolean,
-      default: false,
-    },
+    categories: [{ type: Schema.Types.ObjectId, ref: "PostCategories" }],
   },
   { timestamps: true }
 );
-
-export default mongoose.model("Posts", Post);
+// so now we gonna create a relation between our post and comment
+//here we've no reference to the comment schema that's why we creating the virtual relation
+//for more details see: https://mongoosejs.com/docs/tutorials/virtuals.html in Populate part
+postSchema.virtual("comments", {
+  //mongoDb generate _id for each schema by default the localField here
+  // correspond to our PostSchema id
+  localField: "_id",
+  //and foreign field to the post in my comment Schema
+  foreignField: "post",
+});
+const Post = mongoose.model("Posts", postSchema);
+export default Post;
